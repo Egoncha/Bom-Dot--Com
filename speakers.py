2d99cd2a4ddd429c83a9c1b4a5c1bdd86109800c
egoncha <glezemx2014@gmail.com>
2026-01-30 17:25:42 -0500

    audio files

--- speakers.py
+++ speakers.py
@@ -1,1 +1,22 @@
-//speaker file
+#speaker file
+
+import os
+
+def play(musicFile):
+    os.system(f"aplay {musicFile}")
+
+#py game implementation
+
+# import pygame 
+# import time 
+
+#
+# pygame.mixer.init()
+# pygame.mixer.music.load("music.mp3")
+# pygame.mixer.music.play()
+#
+# time.sleep(10)          # play duration in seconds
+#
+# pygame.mixer.music.stop()
+# pygame.mixer.quit()
+
